354cc887aa0519fc1b53970e61c0d199b5b343d3
hdfsyu <viyaanchauhan@hotmail.com>
2022-04-28 11:45:48 -0400

    added files via git

---
+++ README.md
@@ -0,0 +1,6 @@
+# Raylib Shapes Example
+this is an example of how to render shapes in raylib.
+# Compiling
+compile using `cc main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o main.exe`
+# Running
+i wont put anything on releases, you have to compile it yourself. when you are done compiling run the main.exe file the compiler made.
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <raylib.h>
+const int width = 1280;
+const int height = 720;
+static float rotation = {0.0f};
+static float timer = 0.0f;
+static int sides = 0;
+int main(){
+    InitWindow(width, height, "game");
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+        rotation += 1.0f;
+        timer += GetFrameTime();
+        if(timer > 0.2f){
+            timer = 0.0f;
+            sides++;
+        }
+        // Shapes and I won't show how to render textures because i'm lazy
+        DrawRectangle(width / 2, height / 2, 300, 100, BLUE);
+        DrawRectangleV(Vector2{300, 200}, Vector2{100, 50}, GREEN);
+        DrawRectangleRec(Rectangle{500, 400, 200, 200}, GRAY);
+        DrawRectanglePro(Rectangle{200, 300, 200, 200}, Vector2{50, 50}, rotation, YELLOW);
+        DrawCircle(300, 200, 64, BLACK);
+        DrawTriangle(Vector2{120, 300}, Vector2{0, 100}, Vector2{100, 100}, BROWN);
+        DrawRectangleGradientEx(Rectangle{100, 400, 400, 200}, BROWN, YELLOW, BLUE, GREEN);
+        DrawRectangleLinesEx(Rectangle{400, 300, 100, 100}, 5, BLACK);
+        DrawText("raylib", 408, 375, 20, BLACK);
+        DrawPoly(Vector2{300, 300}, 6, 50, 0.0, PURPLE);
+        DrawPoly(Vector2{100, 400}, sides % 12, 50, 0.0, PINK);
+        EndDrawing();
+    }
+}
